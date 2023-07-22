c9d39e14e29b665acfe140b89d22c10e3001cf34
Soung Oo Lwin <soungoolwin275@gmail.com>
2023-07-22 20:02:44 +0700

    create habit api done

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-white shadow-lg flex-1 mx-4 p-8 mt-[17%] rounded-xl">
     <h2 class="formheader">Create Habit</h2>
-    <form>
+    <form @submit.prevent="createHabit()">
       <div class="mb-4">
         <label class="formlabel" for="habit-title">Habit Title</label>
         <input
@@ -47,6 +47,7 @@
           class="forminput"
           id="time-to-send-reminder"
           name="time-to-send-reminder"
+          v-model="setReminder"
           required
         >
           <option value="6am" selected>6am</option>
@@ -54,8 +55,12 @@
           <option value="6pm">6pm</option>
         </select>
       </div>
+      <div v-if="error" class="text-red-500">
+        {{ error }}
+      </div>
       <button
         class="bg-[#61C9A8] hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
+        type="submit"
       >
         Create
       </button>
@@ -64,7 +69,47 @@
 </template>
 
 <script>
-export default {};
+import { ref } from "vue";
+import axios from "axios";
+import { useRouter } from "vue-router";
+export default {
+  setup() {
+    let router = useRouter();
+    let habitTitle = ref("");
+    let habitDescription = ref("");
+    let startDate = ref("");
+    let setReminder = ref("");
+    let error = ref("");
+
+    let createHabit = async () => {
+      try {
+        let response = await axios.post("/api/v1/habit/createHabit", {
+          habitTitle: habitTitle.value,
+          habitDescription: habitDescription.value,
+          startDate: startDate.value,
+          timeToSendReminder: setReminder.value,
+        });
+
+        if (response.status == 201) {
+          router.push("/profile");
+        } else {
+          error.value = response.data.message;
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    return {
+      createHabit,
+      habitTitle,
+      habitDescription,
+      startDate,
+      setReminder,
+      error,
+    };
+  },
+};
 </script>
 
 <style></style>
